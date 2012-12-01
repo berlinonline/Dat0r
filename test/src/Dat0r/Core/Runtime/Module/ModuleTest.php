448084dc27924c725bfde43fba300c512c7578f2
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2012-12-01 22:11:32 +0000

    adjusted the module test to consider the fact that we've removed the id and the revision fields from the RootModule class.

--- test/src/Dat0r/Core/Runtime/Module/ModuleTest.php
+++ test/src/Dat0r/Core/Runtime/Module/ModuleTest.php
@@ -13,10 +13,7 @@ class ModuleTest extends BaseTest
         $module = RootModule::getInstance();
 
         $this->assertEquals('Article', $module->getName());
-        // All RootModules should own fields for id and revision.
-        $this->assertEquals(5, $module->getFields()->getSize());
-        $this->assertInstanceOf('Dat0r\\Core\\Runtime\\Field\\TextField', $module->getField('id'));
-        $this->assertInstanceOf('Dat0r\\Core\\Runtime\\Field\\TextField', $module->getField('revision'));
+        $this->assertEquals(3, $module->getFields()->getSize());
     }
 
     public function testCreateAggegateModule()
@@ -60,7 +57,7 @@ class ModuleTest extends BaseTest
         $fields = $module->getFields();
 
         $this->assertInstanceOf('Dat0r\\Core\\Runtime\\Field\\FieldCollection', $fields);
-        $this->assertEquals(5, $fields->getSize()); // RootModule's ship with an id and a revision field.
+        $this->assertEquals(3, $fields->getSize()); // RootModule's ship with an id and a revision field.
 
         $this->assertInstanceOf('Dat0r\\Core\\Runtime\\Field\\TextField', $fields->get('headline'));
         $this->assertInstanceOf('Dat0r\\Core\\Runtime\\Field\\TextField', $fields->get('content'));
