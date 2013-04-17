e60f539da2c3fd19be7b6aa4fb18467c66ea2b49
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2013-04-17 22:24:27 +0200

    added missing closing brace...

--- test/src/Dat0r/Core/Runtime/Field/UuidFieldTest.php
+++ test/src/Dat0r/Core/Runtime/Field/UuidFieldTest.php
@@ -27,7 +27,7 @@ class UuidFieldTest extends Core\BaseTest
             '/^\{?[0-9a-f]{8}\-?[0-9a-f]{4}\-?[0-9a-f]{4}\-?'.
             '[0-9a-f]{4}\-?[0-9a-f]{12}\}?$/i', 
             $defaultValue
-        ));
+        )));
     }
 
     /**
