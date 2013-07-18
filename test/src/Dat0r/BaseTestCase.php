2d96220e4ea37671f5299dabc14aff33fc97c829
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2013-07-18 18:16:36 +0200

    the ObjectTest now uses random values for testing.

--- test/src/Dat0r/BaseTestCase.php
+++ test/src/Dat0r/BaseTestCase.php
@@ -2,6 +2,14 @@
 
 namespace Dat0r\Tests;
 
+use Faker;
+
 abstract class BaseTestCase extends \PHPUnit_Framework_TestCase
 {
+    protected $faker;
+
+    public function setUp()
+    {
+        $this->faker = Faker\Factory::create();
+    }
 }
--- test/src/Dat0r/ObjectTest.php
+++ test/src/Dat0r/ObjectTest.php
@@ -9,33 +9,30 @@ class ObjectTest extends BaseTestCase
 {
     public function testCreate()
     {
-        $property_one = 'value one';
-        $property_two = 2;
-        $property_three = false;
-
-        $test_object = Fixtures\TestObject::create(array(
-            'property_one' => $property_one,
-            'property_two' => $property_two,
-            'property_three' => $property_three
-        ));
+        $object_data = $this->getRandomScalarValues();
+        $test_object = Fixtures\TestObject::create($object_data);
 
         $this->assertInstanceOf('\\Dat0r\\IObject', $test_object);
         $this->assertInstanceOf('\\Dat0r\\Tests\\Fixtures\\TestObject', $test_object);
-        $this->assertEquals($property_one, $test_object->getPropertyOne());
-        $this->assertEquals($property_two, $test_object->getPropertyTwo());
-        $this->assertEquals($property_three, $test_object->getPropertyThree());
+        $this->assertEquals($object_data['property_one'], $test_object->getPropertyOne());
+        $this->assertEquals($object_data['property_two'], $test_object->getPropertyTwo());
+        $this->assertEquals($object_data['property_three'], $test_object->getPropertyThree());
     }
 
     public function testToArray()
     {
-        $object_data = array(
-            'property_one' => 'wat?! I can haz valuez?',
-            'property_two' => 3.0,
-            'property_three' => true
-        );
-
+        $object_data = $this->getRandomScalarValues();
         $test_object = Fixtures\TestObject::create($object_data);
 
         $this->assertEquals($object_data, $test_object->toArray());
+    }
+
+    protected function getRandomScalarValues()
+    {
+        return array(
+            'property_one' => $this->faker->word(23),
+            'property_two' => $this->faker->randomNumber(0, 500),
+            'property_three' => $this->faker->boolean()
+        );
     }
 }
