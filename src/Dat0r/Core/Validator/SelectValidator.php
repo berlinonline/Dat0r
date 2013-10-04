2f6dc54b4aa0d71f8d01bf122ee38de05e99455f
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2013-10-04 23:33:29 +0200

    initially adding email and select field basics.

---
+++ src/Dat0r/Core/Validator/SelectValidator.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Dat0r\Core\Validator;
+
+/**
+ * Default implementation for validators that validate text.
+ *
+ * @copyright BerlinOnline Stadtportal GmbH & Co. KG
+ * @author Thorsten Schmitt-Rink <tschmittrink@gmail.com>
+ */
+class SelectValidator extends TextValidator
+{
+    /**
+     * Validates a given value thereby considering the state of the field
+     * that a specific validator instance is related to.
+     *
+     * @param mixed $value
+     *
+     * @return boolean
+     */
+    public function validate($value)
+    {
+        $multi_select_enabled = $this->getField()->getOption('multiple', false);
+        $allowed_options = $this->getField()->getOption('options', array());
+
+        if ($multi_select_enabled) {
+            if (!is_array($value) && !is_null($value)) {
+                return false;
+            } elseif (is_array($value)) {
+                foreach ($value as $option) {
+                    if (!$this->validateOptionValue($option)) {
+                        return false;
+                    }
+                }
+            }
+        } else {
+            if (!is_scalar($value)) {
+                return false;
+            } else {
+                if (!$this->validateOptionValue($value)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    protected function validateOptionValue($value)
+    {
+        $allowed_options = $this->getField()->getOption('options', array());
+        if (!is_scalar($value)) {
+            return false;
+        } else {
+            return empty($value) || in_array($value, $allowed_options);
+        }
+    }
+}
