f294de5d0387111d4ba76ee77056a07034a7a776
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2014-02-22 22:54:11 +0100

    moved uuid generation to dedicated class

--- src/Dat0r/Runtime/Field/Type/UuidField.php
+++ src/Dat0r/Runtime/Field/Type/UuidField.php
@@ -2,6 +2,8 @@
 
 namespace Dat0r\Runtime\Field\Type;
 
+use Dat0r\Common\Uuid;
+
 class UuidField extends TextField
 {
     /**
@@ -11,31 +13,6 @@ class UuidField extends TextField
      */
     public function getDefaultValue()
     {
-        return $this->generateUuidV4();
-    }
-
-    // from http://www.php.net/manual/en/function.uniqid.php#94959
-    // more info http://en.wikipedia.org/wiki/Universally_unique_identifier#Version_4_.28random.29
-    protected function generateUuidV4()
-    {
-        return sprintf(
-            '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-            // 32 bits for "time_low"
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff),
-            // 16 bits for "time_mid"
-            mt_rand(0, 0xffff),
-            // 16 bits for "time_hi_and_version",
-            // four most significant bits holds version number 4
-            mt_rand(0, 0x0fff) | 0x4000,
-            // 16 bits, 8 bits for "clk_seq_hi_res",
-            // 8 bits for "clk_seq_low",
-            // two most significant bits holds zero and one for variant DCE1.1
-            mt_rand(0, 0x3fff) | 0x8000,
-            // 48 bits for "node"
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff)
-        );
+        return Uuid::generateVersion4();
     }
 }
