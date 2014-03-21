74f6572f67d855ed1221c81c3f28a308d7376044
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2014-03-21 21:27:51 +0100

    added first draft url rule

---
+++ src/Dat0r/Runtime/Validator/Rule/Type/UrlRule.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Dat0r\Runtime\Validator\Rule\Type;
+
+use Dat0r\Runtime\Validator\Rule\Rule;
+use Dat0r\Runtime\Validator\Result\IIncident;
+
+class UrlRule extends Rule
+{
+    protected function execute($value)
+    {
+        if (!is_string($value)) {
+            $this->throwError('invalid_type', array(), IIncident::CRITICAL);
+            return false;
+        }
+
+        $success = filter_var($value, FILTER_VALIDATE_URL);
+        if (!$success) {
+            $this->throwError('invalid_format');
+            $success = false;
+        } else {
+            $this->setSanitizedValue($value);
+        }
+
+        return $success;
+    }
+}
